1d431c6aec8a57ff48c8342345dbd148899683d2
Ilja Dubinin <iliadubinin91@gmail.com>
2018-04-19 22:22:58 +0100

    use const and add logging

--- Humidity/CheckSoil/CheckSoil.ino
+++ Humidity/CheckSoil/CheckSoil.ino
@@ -1,14 +1,15 @@
 // YL-39 + HC-68 humidity sensor
 
-#define vetDelay 43200000 // wait if all sensors are wet
-#define dryDelay 180000 // wait if some sensors are dry (probably someone will wet soil)
-#define humidityThreshold 500 // minimum value for "vet"
-#define sensorReadDelay 1000 // defines how long sensor read operation takes
+// some constants
+const unsigned long vetDelay = 43200000; // wait if all sensors are wet
+const unsigned long dryDelay = 1800000; // wait if some sensors are dry (probably someone will wet soil)
+const int humidityThreshold = 500; // minimum value for "vet"
+const int sensorReadDelay = 1000; // defines how long sensor read operation takes
 
-// Register pins
-#define latchPin 11 // (11) ST_CP [RCK] on 74HC595
-#define clockPin 9 // (9) SH_CP [SCK] on 74HC595
-#define dataPin 12 // (12) DS [S1] on 74HC595
+// Register 74HC595 pins
+const int latchPin = 11; // (11) ST_CP [RCK] on 74HC595
+const int clockPin = 9; // (9) SH_CP [SCK] on 74HC595
+const int dataPin = 12; // (12) DS [S1] on 74HC595
 
 // Mappings between analog and digital ports for sensors
 const byte pins[] = {A0, A1, A2, A3};
@@ -40,28 +41,28 @@ void loop() {
   for (int i = 0; i < 4; i++) {
     int humidity = readHumidity(pins[i], vccs[i]);
     Serial.print("Humidity Level ");
-    Serial.print(i);
+    Serial.print(i + 1);
     Serial.print(" ");
     Serial.println(humidity);
-    if (humidity < humidityThreshold && humidity > 10) {
-      bitSet(leds, 4 + i); // left for bits are meaningfull here
+    if (humidity < humidityThreshold && humidity > 20) {
+      bitSet(leds, 4 + i); // left bits are meaningfull here
       status = false;
     }
   }
   lightLeds(leds);
-
   if (status) {
+    Serial.print("All plants are vet. Sleep for: ");
+    Serial.print(vetDelay / 1000 / 60);
+    Serial.println(" mins.");
     delay(vetDelay);
   } else {
+    Serial.print("Some plants are dry. Sleep for: ");
+    Serial.print(dryDelay / 1000 / 60);
+    Serial.println(" mins.");
     delay(dryDelay);
   }
 }
 
-/*
- # 0 ~300 dry soil
- # 300~700 humid soil
- # 700~950 in water
-*/
 int readHumidity(byte pin, byte vcc) {
   digitalWrite(vcc, HIGH);
   delay(sensorReadDelay);
